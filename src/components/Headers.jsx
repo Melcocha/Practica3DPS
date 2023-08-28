6bc745a6097f4285641fc5eeb82188562b17134a
melvin.rodriguez <mr.arteaga06@gmail.com>
2023-08-28 16:57:34 -0600

    commit 28-08-2023

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState } from 'react';
 export const Headers=({
     allProducts,
@@ -10,13 +9,15 @@ export const Headers=({
     
 })=>{
     const [active, setActive] = useState(false);
+
     const onDeleteProduct = product => {
-    const results = allProducts.filter(
-    item => item.id !== product.id
+        const results = allProducts.filter(
+            item => item.id !== product.id
     );
-    setTotal(total - product.price * product.quantity);
-    setCountProducts(countProducts - product.quantity);
-    setAllProducts(results);
+
+        setTotal(total - product.price * product.quantity);
+        setCountProducts(countProducts - product.quantity);
+        setAllProducts(results);
     };
     const onCleanCart = () => {
         setAllProducts([]);
@@ -29,7 +30,10 @@ return (
 <header>
     <h1>Tienda de Libros</h1>
     <div className="container-icon">
-        <div className="container-cart-icon" onClick={() => setActive(!active)}>
+        <div 
+        className="container-cart-icon" 
+        onClick={() => setActive(!active)}
+        >
             <img src="https://e7.pngegg.com/pngimages/833/426/png-clipart-black-shopping-cart-icon-for-free-black-shopping-cart.png" alt="carrito" className="icon-cart" />
                 <div className="count-products">
                     <span id="contador-productos">0</span>
